e67bafab51e0a8ce179ccc95e5832845db76704c
leejua1 <1479q@naver.com>
2020-08-07 12:15:15 +0900

    1:1 peer connection success

--- server/PcServer.js
+++ server/PcServer.js
@@ -1,30 +1,38 @@
-const io = require('socket.io').listen(3100 ,()=>{
+
+const io = require('socket.io').listen(process.env.PORT ,(res)=>{
+    res.header('Access-Control-Allow-Origin', '*');
     console.log('server is running on port 3100')
 })
 let roomName = ""
 io.on('connection', socket=> {
     console.log(`socket is connected with client id ${socket.id}`)
-    socket.on('connect', room=>{//클라이언트가 연결됐을때
-        const clientsInRoom = io.sockets.adapter.rooms[room];//룸 안에 있는 클라이언트
+
+    socket.on('joinRoom', room=>{
+        console.log("client connect")
+        const clientsInRoom = io.sockets.adapter.rooms[room];
         const numClient = clientsInRoom? Object.keys(clientsInRoom.sockets).length : 0;
+        console.log(`client num + ${numClient}`)
         if (numClient ===0){
             socket.join(room)
-            console.log(`first client join socketid ${socket.id}`)
+            console.log(`first client join roomName ${room} socketid ${socket.id}`)
         }else if (numClient ===1){
             socket.join(room)
-            console.log(`second client join socketid ${socket.id}`)
+            console.log(`second client join roomName ${room} socketid ${socket.id}`)
             socket.in(room).emit('letOffer')
         }else{
             socket.in(room).emit('full')
-
         }    })
     socket.on('message',message=>{
         if (message.type==='offer'){
-            socket.in(roomName).emit('recOffer',message)
+            console.log('client send message type offer')
+            socket.broadcast.to("room").emit('recOffer',message)
         }else if (message.type ==='answer'){
-            socket.in(roomName).emit('recAnswer',message)
+            console.log('client send message type answer')
+            socket.broadcast.to("room").emit('recAnswer',message)
         }else if (message.type ==='candidate'){
-            socket.in(roomName).emit('recCandidate')
+            console.log('client send message type candidate')
+            socket.broadcast.to("room").emit('recCandidate', message)
+        }
 
-        }    })
+    })
 })
